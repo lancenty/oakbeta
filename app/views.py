5ecb5db7fc6ae60e9f17c031f1867e34c32e42d2
yangyuan <lancent.yuan@gmail.com>
2014-12-03 12:39:57 +0100

    add two files

---
+++ app/views.py
@@ -0,0 +1,46 @@
+__author__ = 'Lancent'
+
+from flask import render_template, flash, redirect
+from app import app, lm
+from .forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Yang'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'nickname': 'Vittorio'},
+            'body': 'Vaffanculo!'
+        }
+    ]
+    return render_template("index.html",
+        title = 'Home',
+        user = user,
+        posts = posts)
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+        title='Sign In',
+        form=form,
+        providers=app.config['OPENID_PROVIDERS'])
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
---
+++ config.py
@@ -0,0 +1,19 @@
+__author__ = 'Lancent'
+
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+# openid login config
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+# database config
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
