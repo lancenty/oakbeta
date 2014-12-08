2a5b121cf8324b02f67ba59bd8b2b3ab6df5bba4
yangyuan <lancent.yuan@gmail.com>
2014-12-08 22:39:03 +0100

    Finish chapter profile

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,12 @@ from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 db = SQLAlchemy(app)
+
 lm = LoginManager()
 lm.init_app(app)
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+lm.login_view = 'login'
 
 from app import views, models
